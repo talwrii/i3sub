5232d58727c168d80a353715476a00b9fb33fb2c
Tal Wrii <talwrii@gmail.com>
2018-11-17 00:26:39 +0000

    Change bankruptcy: Record all my changes before release.
    Go on, complain good practice on someone else's open source project I
    dare you.

--- i3sub/spawn.py
+++ i3sub/spawn.py
@@ -1,11 +1,64 @@
 import subprocess
+import contextlib
 import time
 import subprocess
 import tempfile
 import sys
 import json
+import i3ipc
 
-def pin_workspace():
+# On the use of polling.
+
+#  I am using polling here rather than creating a connection.
+#  This goes someway to illustrate *why* one would use i3ipc within an application rather on the command-line:
+#       because in practice one must wait for events to avoid race conditions.
+#  Should I make this use i3ipc. This might be odd. But I suppose it isn't really, you can have as many connections to i3ipc as you want
+#  and 3 isn't that many. However, I am feeling lazy at the moment and this is convenience code so polling is fine. Nothing here is
+#  performance or latency sensitive enough that I mind brain-dead polling.
+
+def spawn_on(name, command, conn=None):
+    "Run a command on a workspace."
+    with with_conn(conn) as conn:
+        with with_workspace(name, conn):
+            pin_workspace(conn)
+            subprocess.Popen(command)
+
+@contextlib.contextmanager
+def with_workspace(name, conn=None):
+    "Execute the context on a workspace."
+    with with_conn(conn) as conn:
+        previous = current_workspace(conn)
+        goto_workspace(name, conn)
+        try:
+            yield
+        finally:
+            goto_workspace(previous, conn)
+
+# snippet: withconn: with with_conn(conn) as conn:
+# snippet: cen: conn=None
+
+def wait_for_workspace(name, conn=None):
+    "Wait until a given workspace is selected."
+    for i in range(100):
+        current = current_workspace(conn)
+        if current == name:
+            break
+        time.sleep(0.01)
+    else:
+        raise Exception((name, current))
+
+def goto_workspace(name, conn=None):
+    "Go to a workspace called name."
+    with with_conn(conn) as conn:
+        subprocess.check_call(['i3-msg', 'workspace {}'.format(name)])
+    wait_for_workspace(name)
+
+def current_workspace(conn=None):
+    "Return the name of the current workspace."
+    with with_conn(conn) as conn:
+        return conn.get_tree().find_focused().workspace().name
+
+def pin_workspace(conn=None):
     "Ensure that a workspace does get deleted (Use a layout with a universal swallowing window)."
     layout = '''
 // vim:ts=4:sw=4:et
@@ -32,22 +85,40 @@ def pin_workspace():
     "type": "con"
 }
     '''
-    with tempfile.NamedTemporaryFile(delete=False) as temp:
-        temp.write(layout.encode('utf8'))
-        temp.close()
-        subprocess.check_call([b"i3-msg", "append_layout {}".format(temp.name).encode('utf8')])
-    time.sleep(0.5)
-    # stupid race condition on append_lay
 
+    with with_conn(conn) as conn:
+        workspace = current_workspace(conn)
+        with tempfile.NamedTemporaryFile(delete=False) as temp:
+            temp.write(layout.encode('utf8'))
+            temp.close()
+            subprocess.check_call([b"i3-msg", "append_layout {}".format(temp.name).encode('utf8')])
+        # stupid race condition on append_lay
+        for i in range(100):
+            # This could be done with a subscription... but that does kind of undermine the purpose of this tool!
+            tree = conn.get_tree()
+            if tree.descendents():
+                break
+            time.sleep(0.01)
+
+@contextlib.contextmanager
+def with_conn(given_conn=None):
+    "Connect and clean up the connection."
+    conn = given_conn or i3ipc.Connection()
+    try:
+        yield conn
+    finally:
+        if not given_conn:
+            conn.cmd_socket.close()
 
 def run(event, *args):
-    "Run a command in the context of an event"
+    "Run a command in the context of an event."
     pin_workspace()
     command = ["i3-msg", "workspace {}; exec {}".format(event["current"]["name"], ' '.join(args))]
     print("Running", command)
     subprocess.Popen(command)
 
 def read_events_stdin():
+    "Return an iterator that yields dictionaries representing an i3 bus event."
     while True:
         line = sys.stdin.readline()
         if not line:
@@ -57,4 +128,3 @@ def read_events_stdin():
         except:
             raise Exception("Could not parse {}".format(line))
         yield data
-
